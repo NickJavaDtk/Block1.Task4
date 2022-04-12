ffc03e763785de76d419fe6ce5604d1c0549640a
Nickolay Petrushin <nick.mflj@gmail.com>
2022-04-12 22:47:11 +0300

    commit before creating a remote repository

---
+++ Education.java
@@ -0,0 +1,8 @@
+package Study.stream.task2;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ Sex.java
@@ -0,0 +1,6 @@
+package Study.stream.task2;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
